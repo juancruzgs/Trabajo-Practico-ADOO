12bba076f2c566479dfd94218bf64d2cb7b5e64e
maxes2 <r.maximiliano@hotmail.com>
2014-11-13 05:38:31 -0300

    MOD_broker
    juancho lta
    chorch fuhrer

--- ServerApplication/src/serverapplication/Broker.java
+++ ServerApplication/src/serverapplication/Broker.java
@@ -21,7 +21,9 @@ public class Broker {
             
             Class.forName("com.mysql.jdbc.Driver").newInstance();
             Connection connection = DriverManager.getConnection(url,user,password);
-            MessageAck mensaje = add("pelaterlareconchadetumadreeeeeeeeeeeeeeeeeeeeee","lalo",connection);
+           // MessageAck mensaje = add("pelaterlareconchadetumadreeeeeeeeeeeeeeeeeeeeee","lalo",connection);
+           // MessageAck mensaje = remove("aquiles",connection);
+            MessageAck mensaje = modify("juan","lalo","pelater2014",connection);
             System.out.println(mensaje.getStatus()+" "+mensaje.getDescripcion());
           /*  Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT * FROM usuarios");
@@ -53,6 +55,38 @@ public class Broker {
         catch (SQLException e){ return new MessageAck ("Error",e.getMessage()); }
     }    
     
+    public MessageAck remove(String nombreUsuario,Connection connection){
+        try{
+            Statement statement = connection.createStatement();
+            int resultSet = statement.executeUpdate("DELETE FROM usuarios WHERE username='"+nombreUsuario+"'");
+            if  (resultSet==0){
+                 return new MessageAck("Error","User does not exists");
+            }
+            else{
+                return new MessageAck("OK","");
+            }
+        }catch (SQLException e) { return new MessageAck ("Error",e.getMessage());}
+    
+    }
+    
+    public MessageAck modify(String nombreUsuario,String contraseña, String nuevaContraseña,Connection connection){
+        try{
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT username FROM usuarios WHERE username='"+nombreUsuario+"'");
+             if (resultSet.next()){
+                 int resultSet2 = statement.executeUpdate("UPDATE  usuarios SET password='"+nuevaContraseña+"'WHERE username='"+nombreUsuario+"' and password='"+contraseña+"'");
+                 if (resultSet2!=0){
+                     return new MessageAck("OK","");
+                 }
+                 else{
+                     return new MessageAck("Error","Invalid password");
+                 }
+             }else{
+                 return new MessageAck("Error","User does not exists");
+             }
+             
+        }catch (SQLException e){ return new MessageAck("Error",e.getMessage());}
+    }
     public static Broker getInstance(String url,String user,String password){   
         if (instance == null){
             instance = new Broker(url,user, password);
