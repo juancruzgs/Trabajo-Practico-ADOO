eebc97dec81676f62a23c25f29da18541bad2e9f
juancruzgs <juancruz_gs@hotmail.com>
2014-11-12 22:34:29 -0300

    ADD_Classes

---
+++ ServerApplication/src/serverapplication/Broker.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serverapplication;
+
+import java.io.*;
+import java.sql.*;
+/**
+ *
+ * @author Juan
+ */
+public class Broker {
+    /**
+     * Broker Creator
+     */
+    public Broker(String url, String user, String password){
+        try{
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            Connection connection = DriverManager.getConnection(url,user,password);
+            //Statement statement = connection.createStatement();
+            //ResultSet resultSet = statement.executeQuery("SELECT * FROM usuarios");
+            
+            //while (resultSet.next()){
+            //     System.out.println(resultSet.getString(1));
+            //}
+        }
+        catch (Exception ex)
+        { ex.printStackTrace(); }
+    }
+}
---
+++ ServerApplication/src/serverapplication/Server.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serverapplication;
+
+/**
+ *
+ * @author Juan
+ */
+public class Server {
+    
+    Broker broker;
+    int port;
+    
+    public Server(){
+        //getProperties
+        //port = properties.get
+        //broker = new Broker(properties.get(url),user,password);
+    }
+    
+}
